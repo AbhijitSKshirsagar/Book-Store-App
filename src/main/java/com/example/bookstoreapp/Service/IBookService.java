2e5eb801ba49800e6529a273574ae99155726927
AbhijitSKshirsagar <95201386+AbhijitSKshirsagar@users.noreply.github.com>
2022-06-01 23:23:49 +0530

    Delete IBookService.java

--- src/main/java/com/example/bookstoreapp/Service/IBookService.java
+++
@@ -1,9 +0,0 @@
-//package com.example.bookstoreapp.Service;
-//
-//import com.example.bookstoreapp.model.BookData;
-//
-//import java.util.List;
-//
-//public interface IBookService {
-//    List<BookData> getAllbook();
-//}
